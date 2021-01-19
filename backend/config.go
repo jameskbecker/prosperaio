e02cf1d5bcc71e970c5ff5b4a69a3df32116ae2c
James Becker <48602730+jameskbecker@users.noreply.github.com>
2021-01-19 15:44:26 +0000

    Update config.go

--- backend/config.go
+++ backend/config.go
@@ -85,50 +85,6 @@ func loadTXT(path string) ([]byte, error) {
 	return bData, nil
 }
 
-// func setupConfig() error {
-// 	log.Println("Loading User Data...")
-// 	home, err := os.UserHomeDir()
-// 	if err != nil {
-// 		return errors.New("Error Loading Data (E0001)")
-// 	}
-
-// 	for _, site := range sites {
-// 		err = os.MkdirAll(home+"/ProsperAIO/"+site, os.ModePerm)
-// 		if err != nil {
-// 			return errors.New("Error Loading Data (E0002)")
-// 		}
-
-// 		tasksFile, err := os.Create("tasks_default.csv")
-// 		if err != nil {
-
-// 		}
-// 		csvWriter := csv.NewWriter(tasksFile)
-
-// 		var headers []string
-
-// 		switch site {
-// 		case "kickz":
-// 			headers = []string{
-// 				"Product URL",
-// 				"Size Type",
-// 				"Size Code",
-// 				"First Name",
-// 			}
-// 			break
-// 		}
-
-// 		csvWriter.Write(headers)
-
-// 	}
-
-// 	//configFolder, err := os.Stat(home + "/ProsperAIO/slamjam/x")
-// 	// if err != nil {
-// 	// 	return errors.New("Error Loading Data (E0002)")
-// 	// }
-
-// 	return nil
-// }
-
 func dwTask(columns []string) (demandware.Input, error) {
 	billingAdd := demandware.Address{
 		Title:       columns[4],
